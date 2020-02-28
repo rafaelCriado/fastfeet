1ea04a96d58829bd787c7f780bc1ea2134819d3b
Rafael Criado <rafael.marquescriado@gmail.com>
2020-02-28 18:02:38 -0300

    Configuração de email e template de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6cae12af4db18d',
+    pass: '5b223ee8913326',
+  },
+  default: {
+    from: 'Equipe FastFeet <noreply@fastfeet.com>',
+  },
+};
